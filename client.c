f1554481d1581498a5463b339d624a1d73b969ec
Marco Cipriani <marcocipriani.dev@gmail.com>
2021-01-30 14:36:31 +0100

    Server/Client setup
    Comunicazione di messaggio prefissato dal server

--- client.c
+++ client.c
@@ -1,8 +1,54 @@
 #include "headers.h"
 
+#define SERVER_PORT   5193
+
 int main(int argc, char const *argv[]) {
 
-    printf("Hello, I'm a client\n");
+    int sockfd;
+    struct sockaddr_in saddr;
 
-    return 0;
+    int n;
+    char buffer[1025]; // 1024 + \0
+
+printf("[Client] Hello\n");
+
+    /* Socket */
+    if( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ){
+        perror("[Client] Error creating the socket\n");
+        exit(EXIT_FAILURE);
+    }
+printf("[Client] Sockfd is %d\n", sockfd);
+
+    /* Connect */
+    memset((void *)&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = htons(SERVER_PORT);
+    // TODO saddr.sin_addr = htonl("127.0.0.1");
+    inet_pton(AF_INET, "127.0.0.1", &saddr.sin_addr);
+    if( (connect(sockfd, (struct sockaddr *)&saddr, sizeof(saddr))) < 0 ){
+        perror("[Client] Error in connecting to the server\n");
+        exit(EXIT_FAILURE);
+    }
+printf("[Client] Connected to 127.0.0.1 at %d\n", SERVER_PORT);
+
+    /* Job */
+    while( (n = read(sockfd, buffer, 1024)) > 0 ){
+        if(n<0){
+            perror("[Server] Error in read\n");
+            exit(EXIT_FAILURE);
+        }
+
+        buffer[n] = 0; // TODO or '\0'
+        fputs(buffer, stdout);
+    }
+
+
+    /* Close */
+    if( (close(sockfd)) < 0 ){
+        perror("[Client] Error in closing the client socket\n");
+        exit(EXIT_FAILURE);
+    }
+printf("[Client] Bye\n");
+
+    exit(EXIT_SUCCESS);
 }
--- server.c
+++ server.c
@@ -1,8 +1,63 @@
 #include "headers.h"
 
+#define SERVER_PORT 5193
+#define BACKLOG 10
+
 int main(int argc, char const *argv[]) {
 
-    printf("Hello, I'm a server\n");
+    int listensd, connsd;
+    struct sockaddr_in saddr, caddr;
 
-    return 0;
+    if(argc<2){
+        fprintf(stderr, "[Usage] %s msg\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    char *msg = argv[1];
+
+printf("[Server] Hello\n");
+
+    /* Socket - Listen*/
+    if( (listensd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ){
+        perror("[Server] Error in socket\n");
+        exit(EXIT_FAILURE);
+    }
+printf("[Server] Listensd is %d\n", listensd);
+
+    /* Bind */
+    memset((void *)&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = htons(SERVER_PORT);
+    saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    if( (bind(listensd, (struct sockaddr *)&saddr, sizeof(saddr))) < 0 ){
+        perror("[Server] Error in bind\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Listen */
+    if( (listen(listensd, BACKLOG)) < 0 ){
+        perror("[Server] Error in listen");
+        exit(EXIT_FAILURE);
+    }
+printf("[Server] Ready to accept on port %d\n", SERVER_PORT);
+
+    while(1){
+        /* Accept */
+        if( (connsd = accept(listensd, (struct sockaddr *)NULL, NULL)) < 0 ){
+            perror("[Server] Error in listen");
+            exit(EXIT_FAILURE);
+        }
+
+        if( (write(connsd, msg, strlen(msg))) != strlen(msg) ){
+            perror("[Server] Error in write");
+            exit(EXIT_FAILURE);
+        }
+
+        if( (close(connsd)) < 0){
+            perror("[Server] Error in close");
+            exit(EXIT_FAILURE);
+        }
+printf("[Server] Bye\n");
+    }
+
+    exit(EXIT_SUCCESS);
 }
