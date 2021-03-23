7b6ac42d2658bcaa7d26a65f239d5db5ff65439c
Marco Cipriani <marcocipriani.dev@gmail.com>
2021-03-23 20:41:58 +0100

    Sections updated

--- config.h
+++ config.h
@@ -1,6 +1,9 @@
+// Server settings
 #define SERVER_PORT 5193
 #define SERVER_ADDR "127.0.0.1"
+#define BACKLOG 10
+
+// Packet settings
 #define BUFSIZE 1024
 #define HEADERSIZE 5*sizeof(int)
 #define DATASIZE 1008
-#define BACKLOG 10
