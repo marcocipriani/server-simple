6f382c0acf23e2856e92a00f573e6162bb369bc3
Marco Cipriani <marcocipriani.dev@gmail.com>
2021-03-23 23:01:57 +0100

    Common interface unified

--- common.c
+++ common.c
@@ -1,3 +1,4 @@
+#include "headers.h" // TODO everything needed?
 #include "config.h"
 
 struct pkt{
@@ -9,3 +10,12 @@ struct pkt{
     // TODO int winsize;
     char data[DATASIZE];
 };
+
+int check(int exp, const char *msg){
+    if(exp < 0){
+        perror(msg);
+        fprintf(stderr, "Error code %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+    return exp;
+}
