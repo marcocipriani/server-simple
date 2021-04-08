ee90b817256e6f511895c9f1cb909bc277708ee5
Giaco93 <giaco.lore@hotmail.it>
2021-04-08 18:46:45 +0200

    aggiunta writen

--- common.c
+++ common.c
@@ -116,3 +116,22 @@ ssize_t readn(int fd, void *vptr, size_t n) {/* Read "n" bytes from a descriptor
    }
    return (n - nleft);         /* return byte letti */
  }
+
+ ssize_t writen(int fd, const void *vptr, size_t n){  /* Write "n" bytes to a descriptor. */
+     size_t nleft;
+     ssize_t nwritten;
+     const char *ptr;
+     ptr = vptr;
+     nleft = n;
+     while (nleft > 0) {
+         if ( (nwritten = write(fd, ptr, nleft)) <= 0) {
+             if (nwritten < 0 && errno == EINTR)
+                 nwritten = 0;   /* and call write() again */
+             else
+                 return (-1);    /* error */
+          }
+          nleft -= nwritten;
+          ptr += nwritten;
+     }
+     return (n);    /* byte ancora da scrivere*/
+ }
