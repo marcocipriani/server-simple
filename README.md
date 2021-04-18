f8b6c00c8b4bf1df70d8aa545bc80c1cf55303f1
Marco Cipriani <marcocipriani.dev@gmail.com>
2021-04-18 23:49:48 +0200

    Add configuration script

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ Testing for a C server app development.
 ## Installation
 1. Clone repo
 2. Compile with makefile included
-3. Run ```./server [<path>]``` (Default: ~)
-3. Run ```./client [<operation-number>]```
+3. Run ```./config.sh``` to create folders and test files
+4. Run ```./server [<path>]``` (Default: ./server-files/)
+5. Run ```./client [<operation-number>]```
 
 ### Operation numbers
 - **1** list available files
---
+++ config.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir server-files
+mkdir client-files
+echo "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae ipsum sit amet arcu fringilla egestas eget quis neque. Sed auctor sed nisl at pretium. Ut eget faucibus urna. Fusce rhoncus sollicitudin lacus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean turpis velit, congue id odio sit amet, molestie congue tellus. Duis eget velit molestie, pulvinar tellus vel, euismod orci. Donec maximus ante purus, consectetur vulputate orci mollis vitae." > ./server-files/lorem.txt
